3a7167c261e7ae8b6712f8d67914923d373cbcb5
André Gleißner <agleissner@web.de>
2016-11-10 18:57:07 +0100

    index.html und PHP angefangen
    Erweiterungen folgen noch

---
+++ world_data_parser.php
@@ -0,0 +1,20 @@
+<? php
+
+	class WorldDataParser
+	{
+		
+		function parseCSV(){
+			
+			array fgetcsv(resource $handle, int $length = 0, string $delimeter = ",")
+		}
+		
+		function saveXML(){
+			
+		}
+		
+		function printXML)(){
+			
+		}
+	}
+
+?>
