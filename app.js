1a76cf09867a10eb968f41af07136b4c492d4c5a
André Gleißner <agleissner@web.de>
2016-12-01 22:25:45 +0100

    CSV2JSON Umwandlung
    muss nochmal bearbeitet werden!

--- app.js
+++ app.js
@@ -20,10 +20,58 @@ app.use( express.static( path.join(__dirname, "public") ) );
 ****************************** csv2json *********************************
 **************************************************************************/
 
+var json;
+
+//Gibt csv daten hintereinander aus, speichert
+var csvConverter = new Converter({});
+var readStream = require("fs").createReadStream("public/world_data.csv"); //2 unterschiedliche csv Dateien vorhanden. Fragen welche und Verzeichnis!! 
+var writeStream = require("fs").createWriteStream("public/world_data.json");
+readStream.pipe(csvConverter).pipe(writeStream);
+
+//Gibt csv Daten fast gut untereinander aus, in json Variable
+var csvConverter = new Converter({});
+csvConverter.fromFile("./world_data.csv", function (err, result) {
+    json = result;
+    console.log(json);
+
+    //zur Formatierung:
+    //in JSON String umwandeln
+    /*var csvDaten = JSON.stringify
+    ([
+        { resultdata : result[0] },
+        { resultdata : result[1] }
+    ]);
+
+    //in js String umwandeln
+    csvDaten = JSON.parse(csvDaten);
+    console.log(csvDaten);
+    }); */
+    close(); //Bei Abgabe löschen und console.log auskommentieren
+});
+
+
+
 /**************************************************************************
 ********************** handle HTTP METHODS ***********************
 **************************************************************************/
 
+/*var http = require('http');
+
+app.get('/*', function (req, res) {
+    console.log('GET-Anfrage wurde entgegengenommen und wird bearbeitet!');
+    res.sendFile(__dirname + '/index.html');
+    //TODO
+});
+
+app.post('/*', function(req, res) { 
+    //res.sendFile(__dirname + '/index.html');
+	//TODO
+});
+
+app.delete('/*', function(req, res) { 
+    //TODO
+});
+*/
 
 // DO NOT CHANGE!
 // bind server to port
--- public/assets/js/ajax.js
+++ public/assets/js/ajax.js
@@ -1,1 +1,34 @@
 ﻿alert("No ajax calls implemented ;)");
+
+$.ajax({
+    type: "GET",
+    url: "http://localhost:3000/index.html",
+    async: true,
+    success: function (data) {
+                //TODO
+    }, error: function (jgXHR, text, err) {
+                //TODO
+    }
+});
+
+$.ajax({
+    type: "POST",
+    url: "http://localhost:3000/index.html",
+    async: true,
+    success: function (data) {
+        //TODO
+    }, error: function (jgXHR, text, err) {
+        //TODO
+    }
+});
+
+$.ajax({
+    type: "DELETE",
+    url: "http://localhost:3000/index.html",
+    async: true,
+    success: function (data) {
+        //TODO
+    }, error: function (jgXHR, text, err) {
+        //TODO
+    }
+});
