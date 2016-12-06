36bfce44357a733d1e7976653c6b61a806317df3
André Gleißner <agleissner@web.de>
2016-12-06 18:31:48 +0100

    Abfragen soweit fertig, 2 kleine noch --> TODO

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //Gibt csv daten hintereinander aus, speichert
 var csvConverter = new Converter({});
-var readStream = require("fs").createReadStream("public/world_data.csv"); //2 unterschiedliche csv Dateien vorhanden. Fragen welche und Verzeichnis!! 
-var writeStream = require("fs").createWriteStream("public/world_data.json");
+var readStream = require("fs").createReadStream("./world_data.csv"); //2 unterschiedliche csv Dateien vorhanden. Fragen welche und Verzeichnis!! 
+var writeStream = require("fs").createWriteStream("./world_data.json");
 readStream.pipe(csvConverter).pipe(writeStream);
 
 //Gibt csv Daten fast gut untereinander aus, in json Variable
@@ -56,10 +56,13 @@ csvConverter.fromFile("./world_data.csv", function (err, result) {
 ********************** handle HTTP METHODS ***********************
 **************************************************************************/
 
-//var http = require('http');
-//var fs = require('fs');
+/*  Anmerkungen:
+Wenn die response nicht als JSON String gefordert war, res.end(JSON.stringify(var)); --> res.end(var);      :)
+Da nicht explizit gefordert: IDs oder num's mit 0 wurden korrigiert, sodass json[0] die ID 1 hat usw. Wenn nach IDs mit 0 gesucht wird, erscheint auch ein Fehler.
+*/
 
 //GET Calls
+//gibt alle items zurück
 app.get('/items', function (req, res) {
     var allitems = json;
     //console.log(allitems);
@@ -67,6 +70,7 @@ app.get('/items', function (req, res) {
     //res.json(allitems);
 });
 
+//gibt item mit bestimmter ID zurück
 app.get('/items/:id', function (req, res) {
     var id = req.params.id;
 
@@ -82,10 +86,11 @@ app.get('/items/:id', function (req, res) {
     }
 });
 
-//TODO
+//gibt items mit den IDs im gesuchten Intervall zurück
 app.get('/items/:id1/:id2', function (req, res) {
     var id1 = req.params.id1;
     var id2 = req.params.id2;
+
     //Fehler
     if (id1 > json.length || id2 > json.length || id1 <= 0 || id2 <=0) {
         //console.log("Range not possible.");
@@ -93,43 +98,66 @@ app.get('/items/:id1/:id2', function (req, res) {
     }
     else {
         if (id1 = id2) {
-            var pickeditem = json[--req.params.id]
+            var pickeditem = json[--req.params.id1]
             res.end(JSON.stringify(pickeditem));
         }
+        //TODO
         if (id1 > id2) {
-
+            var pickeditem2 = new Array();
+            var p = json[id2];
+            for (var currentID in p) {
+                pickeditem2.push(currentID)
+                currentID++;
+            }
+        res.end(JSON.stringify(pickeditem2));
         }
+        //TODO
         if (id1 < id2) {
-            for (id1 <= id2; id1 < id2; id1++) {
-                var pickeditem1 = json[--req.params.id1]
-                //var pickeditem2 = json[--req.params.id2]
-                console.log(pickeditem1);
-                //console.log(pickeditem2);
-                res.end(JSON.stringify(pickeditem1));
-                //res.end("Ihre gesuchte ID: " + JSON.stringify(pickeditem2));
-            };
+            var pickeditem1 = new Array();
+            for(i = id1; id2 >= i; i++){
+                pickeditem1.push(json[i])
+            }      
         }
-        else { res.end("Range not possible."); };
+        else {
+        res.end("Range not possible."); };
+    //res.end(JSON.stringify(pickeditem2));
     }
 });
 
-//TODO
+//gibt alle Properties zurück
 app.get('/properties', function (req, res) {
-    json = JSON.parse(json);
-    var allproperties = json["properties"];
-    allproperties = JSON.stringify(allproperties);
+    var p = json[0];    //vom ersten Objekt
+    var allproperties = new Array();
+    for (var property in p) {
+        allproperties.push(property)
+        }
+    //console.log(allproperties);
     res.end(JSON.stringify(allproperties));
-
 });
 
-//TODO
+//gibt bestimmte Property zurück (num-te Property)
 app.get('/properties/:num', function (req, res) {
+    var num = req.params.num;
+    var p = json[0];    //vom ersten Objekt
+    var allproperties = new Array();
+    for (var property in p) {
+        allproperties.push(property)    //dem Array hinzufügen
+    }
+
     //Fehler
-    //console.log("No such property available.");
-    //res.end("No such property available.");
+    if (num > allproperties.length || num <=0 ) {
+        //console.log("No such property available.");
+        res.end("No such property available.");
+    }
+    else {
+        allproperties = allproperties[--num];
+        //console.log(allproperties);
+        res.end(JSON.stringify(allproperties));
+    }
 });
 
 //POST Calls
+//fügt neues Land mit 2 Properties hinzu, sucht nicht nach Lücken!
 app.post('/items', function (req, res) {    //form action="..." setzen??
     //festlegen der Parameter auf der Website, ID automatisch um 1 erhöhen
     var id = json.length + 1;
@@ -141,8 +169,8 @@ app.post('/items', function (req, res) {    //form action="..." setzen??
     var newItem = {
         id: id,
         name: name,
-        birth_rate_per_1000: birthrateper1000,
-        cell_phones_per_100: cellphonesper100
+        "birth rate per 1000" : birthrateper1000,
+        "cell phones per 100": cellphonesper100
     }
 
     //Hinzufügen in Array an Stelle id
@@ -152,16 +180,21 @@ app.post('/items', function (req, res) {    //form action="..." setzen??
 });
 
 //DELETE Calls
+//löscht item, welches an letzter Stelle steht
 app.delete('/items', function (req, res) {
     var name = req.params.name;
     var id = json.length;
-    //get.name?? - Name an der Stelle bekommen, wie?
-    json.splice(--id, 1);
-    console.log("Deleted last country: " + name + "!");
+
+    //TODO Namen bekommen an Stelle id
+    // Ansatz
+    //name = json[req.params.id + name];
+
+    //console.log(json);
+    //console.log("Deleted last country: " + name + "!");
     res.end("Deleted last country: " + name + "!");
-    res.end(JSON.stringify(json));
 });
 
+//löscht item mit bestimmter ID
 app.delete('/items/:id', function (req, res) {
     var id = req.params.id;
     //Fehler
@@ -173,8 +206,8 @@ app.delete('/items/:id', function (req, res) {
         //delete json[--req.params.id]
         json.splice(--id, 1);
         //console.log(json);
-        console.log("Item " + ++id + " deleted successfully.");
-        res.end(JSON.stringify(json));
+        //console.log("Item " + ++id + " deleted successfully.");
+        res.end("Item " + ++id + " deleted successfully.");
     }
 });
 
