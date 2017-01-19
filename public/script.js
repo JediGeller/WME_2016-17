6570358044639c7bdc9f77d64bc097a5534ef3d3
André Gleißner <agleissner@web.de>
2017-01-19 18:32:11 +0100

    Beschriftung richtig, select-Box Versuche, Abgabe
    wahrscheinlich

--- public/script.js
+++ public/script.js
@@ -8,32 +8,29 @@ var dataset = [];
 
 //Source: https://bl.ocks.org/d3noob/bdf28027e0ce70bd132edc64f1dd7ea4   bis csv parsing
 //Größe und Abstände festlegen
-var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+var margin = { top: 20, right: 20, bottom: 100, left: 40 },
     width = screen.width * 0.5 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
+    height = 250 - margin.top - margin.bottom;
 
-// set the ranges
+//Bereiche der Achsen
 var x = d3.scaleBand()
           .range([0, width])
           .padding(0.1);
 var y = d3.scaleLinear()
           .range([height, 0]);
 
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
+//Erstellen der Barcharts
 var barchart1 = d3.select("#chart1").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var barchart2 = d3.select("#chart2").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 //csv parsing
@@ -63,6 +60,7 @@ d3.csv("world_data.csv", function (data) {
         d.internet_user_per_100 = +d.internet_user_per_100;
         d.life_expectancy = +d.life_expectancy;
         d.military_expenditure_percent_of_gdp = +d.military_expenditure_percent_of_gdp;
+        //Funktion zum externen benutzen
     });
 
 
@@ -77,6 +75,75 @@ d3.csv("world_data.csv", function (data) {
     //    });
     //});
 
+    var data1 = properties;
+    var data2 = properties;
+
+    var selectedProperty = properties[0];
+ 
+    // Select-Box 1
+    //Source: http://bl.ocks.org/jfreels/6734823
+    var select = d3.select('#selectbox1')
+      .append('select')
+        .attr('class', 'select')
+        .on('change', onchange)
+
+    var options = select
+    .selectAll('option')
+      .data(data1).enter()
+      .append('option')
+          .text(function (d) { return d; });
+
+    //var options = select
+    //  .selectAll('option')
+    //    .data(data1).enter()
+    //    .append('option')
+    //        .text(function (d) {
+    //            return d.id
+    //            d.birth_rate_per_1000
+    //            d.cell_phones_per_100
+    //            d.children_per_woman
+    //            d.electricity_consumption_per_capita
+    //            d.gdp_per_capita
+    //            d.gdp_per_capita_growth
+    //            d.inflation_annual
+    //            d.internet_user_per_100
+    //            d.life_expectancy
+    //            d.military_expenditure_percent_of_gdp;
+    //        });
+
+    // Select-Box 2
+    //Source: http://bl.ocks.org/jfreels/6734823
+    var select2 = d3.select('#selectbox2')
+      .append('select')
+        .attr('class', 'select')
+        .on('change', onchange)
+
+    var options = select2
+      .selectAll('option')
+        .data(data2).enter()
+        .append('option')
+            .text(function (d) { return d; });
+
+    //Funktion um Wert der Select-Box als Attirbut zu nehmen
+    //Funktioniert noch nicht!
+    function onchange() {
+        selectedProperty = d3.select('select').property('value');
+        //optiondata = options[0][selectedIndex].__data__;
+        console.log(selectedProperty);
+        //console.log(optiondata);
+        d3.selectAll("#chart1")
+                .data(data).enter()
+                .append("rect")
+                    .attr("class", "bar")
+                    .attr("x", function (d) { return x(d.name); })
+                    .attr("width", x.bandwidth())
+                    .attr("y", function (d) { return y(selectedProperty); })
+                    .attr("height", function (d) { return height - y(selectedProperty); })
+                    .attr("fill", "grey");
+
+        x.domain(data.map(function (d) { return d.name; }));
+        y.domain([0, d3.max(data, function (d) { return selectedProperty; })]);
+    };
 
     // Barchart 1
     //Source: https://bl.ocks.org/d3noob/bdf28027e0ce70bd132edc64f1dd7ea4
@@ -84,7 +151,7 @@ d3.csv("world_data.csv", function (data) {
     x.domain(data.map(function (d) { return d.name; }));
     y.domain([0, d3.max(data, function (d) { return d.id; })]);
 
-    //Rechteckige Stufen für das Diagramm (rect)
+    //Rechteckige Stufen für das Diagramm (rect) und deren Größe
     barchart1.selectAll("#chart1")
         .data(data)
       .enter().append("rect")
@@ -95,12 +162,17 @@ d3.csv("world_data.csv", function (data) {
         .attr("height", function (d) { return height - y(d.id); })
         .attr("fill", "grey");
 
-    //X-Achse (unten)
+    //add X-Achse (unten)
     barchart1.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+            .attr("y", -5)
+            .attr("x", 8)
+            .attr("transform", "rotate(90)")
+            .style("text-anchor", "start");
 
-    //Y-Achse (links)
+    //add Y-Achse (links)
     barchart1.append("g")
         .call(d3.axisLeft(y));
 
@@ -111,7 +183,7 @@ d3.csv("world_data.csv", function (data) {
     x.domain(data.map(function (d) { return d.name; }));
     y.domain([0, d3.max(data, function (d) { return d.life_expectancy; })]);
 
-    //Rechteckige Stufen für das Diagramm (rect)
+    //Rechteckige Stufen für das Diagramm (rect) und deren Größe
     barchart2.selectAll("#chart2")
         .data(data)
       .enter().append("rect")
@@ -122,52 +194,19 @@ d3.csv("world_data.csv", function (data) {
         .attr("height", function (d) { return height - y(d.life_expectancy); })
         .attr("fill", "grey");
 
-    //X-Achse (unten)
+    //add X-Achse (unten)
     barchart2.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x))
+    .selectAll("text")
+            .attr("y", -5)
+            .attr("x", 8)
+            .attr("transform", "rotate(90)")
+            .style("text-anchor", "start");
 
-    //Y-Achse (links)
+    //add Y-Achse (links)
     barchart2.append("g")
         .call(d3.axisLeft(y));
-
-
-    var data1 = properties;
-    var data2 = properties;
-
-    var selectedProperty = properties[0];
-
-    // Select-Box 1
-    //Source: http://bl.ocks.org/jfreels/6734823
-    var select = d3.select('#selectbox1')
-      .append('select')
-        .attr('class', 'select')
-        .on('change', onchange)
-
-    var options = select
-      .selectAll('option')
-        .data(data1).enter()
-        .append('option')
-            .text(function (d) { return d; });
-
-    // Select-Box 2
-    //Source: http://bl.ocks.org/jfreels/6734823
-    var select = d3.select('#selectbox2')
-      .append('select')
-        .attr('class', 'select')
-        .on('change', onchange)
-
-    var options = select
-      .selectAll('option')
-        .data(data2).enter()
-        .append('option')
-            .text(function (d) { return d; });
-
-    function onchange() {
-        selectedProperty = d3.select('select').property('value');
-        console.log(selectedProperty);
-        //return selectedProperty;
-    };
 
     /**************************************************************************
     ****************************** Leaflet **********************************
